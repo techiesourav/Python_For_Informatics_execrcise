fba5ab24b2f6d3b924a73f41033fabe9ff06ff10
Sourav Ghosh <wbsourav@gmail.com>
2015-06-01 20:12:33 +0530

    Solution to problem execrise 5.1

---
+++ Chapter05/exercise_5.1.py
@@ -0,0 +1,39 @@
+#exercise 5.1
+
+#define a function to ask for user input and validation
+
+def take_user_input():
+	num = 0
+	input = raw_input("Enter a Number:")
+	if input == 'done':
+		return -1
+	try:
+		num = float(input)
+	except:
+		print "Bad Data"
+	return num
+		
+		
+#### The main function body start here
+
+sum = 0
+counter = 0
+average =0
+
+while (1):
+
+	temp = take_user_input();
+
+	if temp > 0:
+		sum += temp
+		counter += 1
+	elif temp != -1:
+		continue
+	else:
+		break
+
+try:
+	average = sum / counter;
+	print sum, counter, average
+except:
+	print "Nothing to do here, Goodbye!!!"
