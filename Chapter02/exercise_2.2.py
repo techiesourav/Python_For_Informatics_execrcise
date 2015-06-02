224c893f2de34939d862ffe63435f9692c5de41a
Natasha Dutta <natasha.ece@gmail.com>
2015-06-03 02:32:11 +0530

    adding solution for assignment 2.2

---
+++ Chapter02/exercise_2.2.py
@@ -0,0 +1,14 @@
+
+#Exercise 2.15
+
+# Exercise 2.2 Write a program that uses raw_input to prompt a user for their
+#              name and then welcomes them.
+
+# Sample Run
+
+#   Enter your name: Chuck
+#   Hello Chuck
+
+
+name = raw_input("What is your name? ")
+print '\nWelcome ', name
