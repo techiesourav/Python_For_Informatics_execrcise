83e99e890f00e5adc2abcd24b6bf6bfe06a49af0
Sourav Ghosh <wbsourav@gmail.com>
2015-06-02 22:08:13 +0530

    added the question itself

--- Chapter05/exercise_5.1.py
+++ Chapter05/exercise_5.1.py
@@ -1,6 +1,19 @@
 #exercise 5.1
 
-#define a function to ask for user input and validation
+# Exercise 5.1 Write a program which repeatedly reads numbers until the user enters
+#              “done”. Once “done” is entered, print out the total, count, and average of
+#               the numbers. If the user enters anything other than a number, detect their mistake
+#               using try and except and print an error message and skip to the next number.
+
+#### Sample Run, Input and output
+
+#		Enter a number: 4
+#		Enter a number: 5
+#		Enter a number: bad data
+#		Invalid input
+#		Enter a number: 7
+#		Enter a number: done
+#		16 3 5.33333333333
 
 def take_user_input():
 	num = 0
